53a8393b29515f18be0ee38cb92f82988ffc3ffd
Caner Alma <61209069+TC851@users.noreply.github.com>
2022-12-22 20:14:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print()
 
 def print_day_of_week(day, month, year):
     date = datetime.date(year, month, day)
-    day_of_week = date.strftime("%A")  # %A: full name of the day of the week (e.g. Monday for Monday)
+    day_of_week = date.strftime("%A")  # %A: full name of the day of the week (e.g.Monday)
     print("The day of the week is:" + " " + str(day_of_week))
 
 
