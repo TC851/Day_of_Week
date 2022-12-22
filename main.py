748a729e3963aa20b204c8c55404f6c8987f54c7
TC851 <caneralma@hotmail.de>
2022-12-22 20:12:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import datetime
+
+print("This program outputs the day of the week for a given date.")
+print()
+
+
+def print_day_of_week(day, month, year):
+    date = datetime.date(year, month, day)
+    day_of_week = date.strftime("%A")  # %A: full name of the day of the week (e.g. Monday for Monday)
+    print("The day of the week is:" + " " + str(day_of_week))
+
+
+# main-------------------------------------------------------------
+print_day_of_week(12, 12, 2050)
